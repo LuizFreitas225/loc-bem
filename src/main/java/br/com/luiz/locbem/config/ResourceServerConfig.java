0ce348e70db42412df053df42103de7fc54fddae
Luiz Nicolau Lemos Freitas <luiznicolaulemosfreitas@gmail.com>
2024-11-06 00:30:45 -0300

    cors configure

--- src/main/java/br/com/luiz/locbem/config/ResourceServerConfig.java
+++ src/main/java/br/com/luiz/locbem/config/ResourceServerConfig.java
@@ -1,10 +1,14 @@
 package br.com.luiz.locbem.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -12,8 +16,22 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors()
+                .and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .antMatchers("/users").authenticated();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Permite todas as origens
+        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
+        config.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
--- src/main/java/br/com/luiz/locbem/config/WebSecurityConfigAdapter.java
+++ src/main/java/br/com/luiz/locbem/config/WebSecurityConfigAdapter.java
@@ -1,16 +1,20 @@
 package br.com.luiz.locbem.config;
 
 import br.com.luiz.locbem.dto.security.UserCustomDTO;
-
 import br.com.luiz.locbem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 
 
 @Configuration
@@ -33,5 +37,25 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().and()  // Ativa o suporte ao CORS
+//                .authorizeRequests()
+//                .anyRequest().authenticated()  // Configura a autenticação para as requisições
+//                .and()
+//                .oauth2Login(); // Configuração para OAuth2
+//    }
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOriginPattern("*"); // Permite todas as origens
+//        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
+//        config.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 }
