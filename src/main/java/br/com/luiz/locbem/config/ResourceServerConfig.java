3b3b7a2877741b2fd6a2003966fc9384d6713484
Luiz Nicolau Lemos Freitas <luiznicolaulemosfreitas@gmail.com>
2024-11-08 00:12:55 -0300

    update doFilter

--- src/main/java/br/com/luiz/locbem/config/ResourceServerConfig.java
+++ src/main/java/br/com/luiz/locbem/config/ResourceServerConfig.java
@@ -2,6 +2,8 @@ package br.com.luiz.locbem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,28 +12,67 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable().authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/users").permitAll()
+//                .antMatchers("/users").authenticated();
+//    }
 
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/users").permitAll()
-                .antMatchers("/users").authenticated();
-    }
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/users").permitAll()
+//                .antMatchers("/users").authenticated()
+//                .antMatchers("/oauth/token").permitAll();;
+//    }
 
 //    @Bean
-//    public CorsFilter corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOriginPattern("*"); // Permite todas as origens
-//        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
-//        config.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
-//        source.registerCorsConfiguration("/**", config);
-//        return new CorsFilter(source);
-//    }
+////    @Order(Ordered.HIGHEST_PRECEDENCE)
+////    public CorsFilter corsFilter() {
+////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+////        CorsConfiguration config = new CorsConfiguration();
+////
+////        config.setAllowCredentials(true); // permite credenciais
+////        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // permite todas as origens
+////        config.setAllowedMethods(Arrays.asList("*")); // permite todos os métodos HTTP
+////        config.setAllowedHeaders(Arrays.asList("*")); // permite todos os headers
+////        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type")); // permite expor certos headers
+////
+////        source.registerCorsConfiguration("/**", config);
+////        return new CorsFilter(source);
+////    }
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true); // permite credenciais
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // permite origem sem barra
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // permite métodos HTTP
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Access-Control-Allow-Headers")); // permite headers
+        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type")); // permite expor headers
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+@Override
+public void configure(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable()
+            .authorizeRequests()
+//            .antMatchers(HttpMethod.POST, "/users").permitAll()
+//            .antMatchers(HttpMethod.GET, "/users").permitAll()
+//            .antMatchers("/users").authenticated()
+//            .antMatchers("/oauth/token").permitAll()
+            .anyRequest().permitAll(); // Permitir o endpoint /oauth/token
+}
+
 }
--- src/main/java/br/com/luiz/locbem/config/WebSecurityConfigAdapter.java
+++ src/main/java/br/com/luiz/locbem/config/WebSecurityConfigAdapter.java
@@ -7,14 +7,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
 
 
 @Configuration
@@ -35,27 +30,5 @@ public class WebSecurityConfigAdapter extends WebSecurityConfigurerAdapter {
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and()  // Ativa o suporte ao CORS
-                .authorizeRequests()
-                .anyRequest().authenticated()  // Configura a autenticação para as requisições
-                .and()
-                .oauth2Login(); // Configuração para OAuth2
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // Permite todas as origens
-        config.addAllowedHeader("*"); // Permite todos os cabeçalhos
-        config.addAllowedMethod("*"); // Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 }
